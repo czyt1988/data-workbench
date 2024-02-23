20971ee18033f4b0bbb5e82fc7487a8c96da16ac
czyt1988 <czy.t@163.com>
2024-02-23 17:28:55 +0800

    临时提交

--- src/DAGraphicsView/DAGraphicsTextItem.cpp
+++ src/DAGraphicsView/DAGraphicsTextItem.cpp
@@ -3,63 +3,122 @@
 #include <QFont>
 #include <QPainter>
 #include <QTextItem>
+#include <QGraphicsSceneResizeEvent>
 #include "DAGraphicsStandardTextItem.h"
 namespace DA
 {
-DAGraphicsTextItem::DAGraphicsTextItem(QGraphicsItem* parent)
-    : DAGraphicsResizeableItem(parent), m_textItem(new DAGraphicsStandardTextItem(this))
+class DAGraphicsTextItem::PrivateData
+{
+    DA_DECLARE_PUBLIC(DAGraphicsTextItem)
+public:
+    PrivateData(DAGraphicsTextItem* p);
+    DAGraphicsStandardTextItem* mTextItem { nullptr };
+    bool mEnableRelativePosition { false };
+    qreal mRelativeX { 0.0 };
+    qreal mRelativeY { 0.0 };
+    Qt::Corner mRelativeCorner { Qt::TopLeftCorner };
+};
+
+DAGraphicsTextItem::PrivateData::PrivateData(DAGraphicsTextItem* p) : q_ptr(p)
+{
+    mTextItem = new DAGraphicsStandardTextItem(p);
+}
+
+//----------------------------------------------------
+// DAGraphicsTextItem
+//----------------------------------------------------
+
+DAGraphicsTextItem::DAGraphicsTextItem(QGraphicsItem* parent) : DAGraphicsResizeableItem(parent), DA_PIMPL_CONSTRUCT
 {
     //    m_textItem->setFocusProxy(this);
     setAcceptDrops(true);
     setAcceptHoverEvents(true);
-    setFocusProxy(m_textItem);
+    setFocusProxy(d_ptr->mTextItem);
 }
 
 DAGraphicsTextItem::DAGraphicsTextItem(const QFont& f, QGraphicsItem* parent)
-    : DAGraphicsResizeableItem(parent), m_textItem(new DAGraphicsStandardTextItem(f, this))
+    : DAGraphicsResizeableItem(parent), DA_PIMPL_CONSTRUCT
 {
     //    m_textItem->setFocusProxy(this);
+    d_ptr->mTextItem->setFont(f);
     setAcceptDrops(true);
     setAcceptHoverEvents(true);
-    setFocusProxy(m_textItem);
+    setFocusProxy(d_ptr->mTextItem);
 }
 
 DAGraphicsTextItem::DAGraphicsTextItem(const QString& str, const QFont& f, QGraphicsItem* parent)
-    : DAGraphicsResizeableItem(parent), m_textItem(new DAGraphicsStandardTextItem(str, f, this))
+    : DAGraphicsResizeableItem(parent), DA_PIMPL_CONSTRUCT
 {
     //    m_textItem->setFocusProxy(this);
+    d_ptr->mTextItem->setFont(f);
+    setText(str);
     setAcceptDrops(true);
     setAcceptHoverEvents(true);
     setFlags(ItemUsesExtendedStyleOption);
-    setFocusProxy(m_textItem);
+    setFocusProxy(d_ptr->mTextItem);
 }
 
 void DAGraphicsTextItem::setEditMode(bool on)
 {
-    m_textItem->setEditMode(on);
+    d_ptr->mTextItem->setEditMode(on);
 }
 
 bool DAGraphicsTextItem::saveToXml(QDomDocument* doc, QDomElement* parentElement) const
 {
     DAGraphicsResizeableItem::saveToXml(doc, parentElement);
-    m_textItem->saveToXml(doc, parentElement);
+    d_ptr->mTextItem->saveToXml(doc, parentElement);
     return true;
 }
 
 bool DAGraphicsTextItem::loadFromXml(const QDomElement* itemElement)
 {
     DAGraphicsResizeableItem::loadFromXml(itemElement);
-    return m_textItem->loadFromXml(itemElement);
+    return d_ptr->mTextItem->loadFromXml(itemElement);
 }
 
 DAGraphicsStandardTextItem* DAGraphicsTextItem::textItem() const
 {
-    return m_textItem;
+    return d_ptr->mTextItem;
 }
 
 void DAGraphicsTextItem::setBodySize(const QSizeF& s)
 {
     DAGraphicsResizeableItem::setBodySize(s);
+    QRectF br = boundingRect();
+    d_ptr->mTextItem->setPos(br.topLeft());
+    d_ptr->mTextItem->setTextWidth(br.width());
+    d_ptr->mTextItem->adjustSize();
+}
+
+void DAGraphicsTextItem::setText(const QString& v)
+{
+    d_ptr->mTextItem->setPlainText(v);
+}
+
+QString DAGraphicsTextItem::getText() const
+{
+    return d_ptr->mTextItem->toPlainText();
+}
+
+void DAGraphicsTextItem::setRelativePosition(qreal xp, qreal yp, Qt::Corner parentCorner)
+{
+    d_ptr->mRelativeX      = xp;
+    d_ptr->mRelativeY      = yp;
+    d_ptr->mRelativeCorner = parentCorner;
+}
+
+void DAGraphicsTextItem::setEnableRelativePosition(bool on)
+{
+    d_ptr->mEnableRelativePosition = on;
+}
+
+bool DAGraphicsTextItem::isEnableRelativePosition() const
+{
+    return d_ptr->mEnableRelativePosition;
+}
+
+void DAGraphicsTextItem::updateRelativePosition()
+{
 }
 
 /**
@@ -71,9 +130,7 @@ void DAGraphicsTextItem::setBodySize(const QSizeF& s)
  */
 void DAGraphicsTextItem::paintBody(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget, const QRectF& bodyRect)
 {
-    // TODO:
-    Q_UNUSED(bodyRect);
-    // m_textItem->paint(painter, option, widget);
+    DAGraphicsResizeableItem::paintBody(painter, option, widget, bodyRect);
 }
 
 }  // end da
--- src/DAGraphicsView/DAGraphicsTextItem.h
+++ src/DAGraphicsView/DAGraphicsTextItem.h
@@ -1,6 +1,5 @@
 #ifndef DAGRAPHICSTEXTITEM_H
 #define DAGRAPHICSTEXTITEM_H
-
 #include <QGraphicsTextItem>
 #include "DAGraphicsResizeableItem.h"
 namespace DA
@@ -12,6 +11,7 @@ class DAGraphicsStandardTextItem;
 class DAGRAPHICSVIEW_API DAGraphicsTextItem : public DAGraphicsResizeableItem
 {
     Q_OBJECT
+    DA_DECLARE_PRIVATE(DAGraphicsTextItem)
 public:
     /**
      * @brief 适用qgraphicsitem_cast
@@ -37,13 +37,51 @@ public:
 
     // 获取内部的文本item
     DAGraphicsStandardTextItem* textItem() const;
+    // 设置尺寸这里的尺寸是不包括旋转和缩放的辅助控制，如果构造函数中需要设置默认大小，使用changeBodySize
+    void setBodySize(const QSizeF& s) override;
+
+    /**
+     * @brief 设置文本
+     * @param v
+     */
+    void setText(const QString& v);
+
+    /**
+     * @brief 文本
+     * @return
+     */
+    QString getText() const;
+
+    /**
+     * @brief 设置相对父窗口的相对定位
+     * @param xp x位置相对父级item的宽度占比，如parentCorner=Qt::TopLeftCorner,xp=0.2
+     * 那么textitem的x定位设置为parentItem.boundingRect().topLeft().x() + parentItem.width() * xp
+     * @param yp y位置相对父级item的宽度占比，如parentCorner=Qt::TopLeftCorner,yp=0.1
+     * 那么textitem的y定位设置为parentItem.boundingRect().topLeft().y() + parentItem.width() * yp
+     * @param parentCorner 相对定位点
+     */
+    void setRelativePosition(qreal xp, qreal yp, Qt::Corner parentCorner);
+
+    /**
+     * @brief 设置是否开启相对定位
+     * @note 相对定位需要有父级item，否则无效
+     * @param on
+     */
+    void setEnableRelativePosition(bool on);
+
+    /**
+     * @brief 是否开启相对定位
+     * @return
+     */
+    bool isEnableRelativePosition() const;
+
+    /**
+     * @brief 更新相对位置
+     */
+    void updateRelativePosition();
 
 protected:
-    void setBodySize(const QSizeF& s) override;
     void paintBody(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget, const QRectF& bodyRect) override;
-
-private:
-    DAGraphicsStandardTextItem* m_textItem;
 };
 }
 #endif  // DAGRAPHICSTEXTITEM_H
