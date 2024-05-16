e2bcca2a783b6deb8a323f24eac7321a3a87131e
czyt1988 <czy.t@163.com>
2024-05-17 00:35:46 +0800

    针对固有插件的cmake进行了调整

--- cmake/daworkbench_plugin_utils.cmake
+++ cmake/daworkbench_plugin_utils.cmake
@@ -1,4 +1,4 @@
-macro(damacro_set_bin_name _var)
+﻿macro(damacro_set_bin_name _var)
     set(DA_MIN_QT_VERSION 5.14)
     find_package(QT NAMES Qt6 Qt5 COMPONENTS Core REQUIRED)
     ########################################################
@@ -78,6 +78,67 @@ macro(damacro_plugin_setting _plugin_name _plugin_description _plugin_ver_major 
     message(STATUS "  | => PLUGIN CMAKE_CURRENT_BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR}")
 endmacro(damacro_plugin_setting)
 
+
+# 定义DA_PLUGIN*的宏
+# _plugin_name lib的名字，决定变量DA_PLUGIN_NAME
+# _plugin_description lib的描述，决定变量DA_PLUGIN_DESCRIPTION
+# _plugin_ver_major lib的主版本号，决定变量DA_PLUGIN_VERSION_MAJOR
+# _plugin_ver_minor lib的次版本号，决定变量DA_PLUGIN_VERSION_MINOR
+# _plugin_ver_path lib的末版本号，决定变量DA_PLUGIN_VERSION_PATCH
+# 生成：DA_PLUGIN_VERSION，完整的版本名
+# 生成：DA_PLUGIN_FULL_DESCRIPTION，完整的描述
+# 生成：DA_MIN_QT_VERSION 最低qt版本要求
+macro(_damacro_plugin_setting _plugin_name _plugin_description _plugin_ver_major _plugin_ver_minor _plugin_ver_path)
+    set(DA_MIN_QT_VERSION 5.14)
+        set(DA_PLUGIN_NAME ${_plugin_name})
+    set(DA_PLUGIN_DESCRIPTION ${_plugin_description})
+    set(DA_PLUGIN_VERSION_MAJOR ${_plugin_ver_major})
+    set(DA_PLUGIN_VERSION_MINOR ${_plugin_ver_minor})
+    set(DA_PLUGIN_VERSION_PATCH ${_plugin_ver_path})
+    set(DA_PLUGIN_VERSION "${DA_PLUGIN_VERSION_MAJOR}.${DA_PLUGIN_VERSION_MINOR}.${DA_PLUGIN_VERSION_PATCH}")
+    set(DA_PLUGIN_FULL_DESCRIPTION "${DA_PLUGIN_NAME} ${DA_PLUGIN_VERSION} | ${DA_PLUGIN_DESCRIPTION}")
+
+    project(${DA_PLUGIN_NAME}
+        VERSION ${DA_PLUGIN_VERSION}
+        LANGUAGES CXX
+        DESCRIPTION ${DA_PLUGIN_FULL_DESCRIPTION}
+    )
+    ########################################################
+    # 通用常规设置
+    ########################################################
+    # C++标准要求最低C++17
+    set(CMAKE_CXX_STANDARD 17)
+    set(CMAKE_CXX_STANDARD_REQUIRED ON)
+    # 编译选项
+    set(CMAKE_DEBUG_POSTFIX "d" CACHE STRING "add a postfix, usually d on windows")
+    set(CMAKE_RELEASE_POSTFIX "" CACHE STRING "add a postfix, usually empty on windows")
+    set(CMAKE_RELWITHDEBINFO_POSTFIX "rd" CACHE STRING "add a postfix, usually empty on windows")
+    set(CMAKE_MINSIZEREL_POSTFIX "s" CACHE STRING "add a postfix, usually empty on windows")
+    ########################################################
+    # MSVC设置
+    ########################################################
+    if(MSVC)
+    # msvc utf-8
+        add_compile_options("$<$<C_COMPILER_ID:MSVC>:/utf-8>")
+        add_compile_options("$<$<CXX_COMPILER_ID:MSVC>:/utf-8>")
+    endif()
+    ########################################################
+    # 目录包含
+    ########################################################
+    # 包含自身目录
+    set(CMAKE_INCLUDE_CURRENT_DIR ON)
+    # 默认的CMAKE_INSTALL_PREFIX
+    # 插件的安装路径和DAWorkbench一致，这样才能正确把插件安装
+    ########################################################
+    # 打印信息
+    ########################################################
+    message("")
+    message("${DA_PLUGIN_FULL_DESCRIPTION}")
+    message(STATUS "  | => DA_PLUGIN_NAME=${DA_PLUGIN_NAME}")
+    message(STATUS "  | => PLUGIN CMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}")
+    message(STATUS "  | => PLUGIN CMAKE_CURRENT_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR}")
+    message(STATUS "  | => PLUGIN CMAKE_CURRENT_BINARY_DIR=${CMAKE_CURRENT_BINARY_DIR}")
+endmacro(_damacro_plugin_setting)
 # 获取插件的runtime所在目录，也就是dll放置的位置
 macro(damacro_get_plugin_runtime_install_dir _var_plugin_install_dir _daworkbench_install_root_dir)
     ########################################################
@@ -192,3 +253,6 @@ macro(damacro_import_Python __target_name)
     target_link_libraries(${__target_name} PUBLIC ${Python3_LIBRARIES})
     target_include_directories(${__target_name} PUBLIC ${Python3_INCLUDE_DIRS})
 endmacro(damacro_import_Python)
+
+
+
