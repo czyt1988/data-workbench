44d17a180d4b3e9872a6bb36a815bba1c3ef4ea0
czyt1988 <czy.t@163.com>
2026-09-03 11:36:48 +0800

    docs: revise industrial software agent blog post
    - Rewrite introduction to focus on C++/Qt agent integration
    - Add demo GIFs for agent analysis and auto-charting
    - Simplify and remove redundant sections on architecture and agent principles to make the content more concise

--- src/DAConfigs.h
+++ src/DAConfigs.h
@@ -37,7 +37,7 @@
  * @def 编译的日期（字符串）
  */
 #ifndef DA_COMPILE_DATETIME
-#define DA_COMPILE_DATETIME "260901"
+#define DA_COMPILE_DATETIME "260902"
 #endif
 
 /**
@@ -58,7 +58,7 @@
  * @def 编译的日期（字符串）
  */
 #ifndef DA_COMPILE_DATETIME_DAY
-#define DA_COMPILE_DATETIME_DAY "01"
+#define DA_COMPILE_DATETIME_DAY "02"
 #endif
 
 /**
